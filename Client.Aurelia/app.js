85d7a03b91ea96d44d5e73de6690c9f2ca0b4006
LouisChiero <louis.chiero@outlook.com>
2016-03-09 22:12:36 -0600

    Initial check in
    Initial check in.

---
+++ Client.Aurelia/app.js
@@ -0,0 +1,18 @@
+﻿import {inject} from "aurelia-framework";
+import {HttpClient} from "aurelia-http-client";
+
+@inject(HttpClient)
+export class App {
+
+    constructor() {
+        this.message = "";
+    }
+
+    activate() {
+        this.message = "Aurelia Hello World!";
+    }
+
+    changeMessage () {
+        this.message = "Goodbye!!!";
+    }
+}
